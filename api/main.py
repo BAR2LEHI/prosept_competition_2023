30163f630abacb86acd0b06c95ffed7679e3659c
BAR2LEHI <vat200@yandex.ru>
2023-12-06 13:37:58 +0300

    Add middleware

--- api/main.py
+++ api/main.py
@@ -7,9 +7,19 @@ from .model import model_job
 
 
 origins = [
-    '*',
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:8001",
+    "http://localhost:5173",
+    "http://127.0.0.1:8080",
+    "http://127.0.0.1:8001",
+    "http://127.0.0.1:5173",
+    "http://127.0.0.1:1",
+    "http://localhost:80",
+    "http://127.0.0.1:80",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000"
 ]
-
 
 app = FastAPI()
 
