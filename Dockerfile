14c567b064db620861476f202509a524df1148f6
BAR2LEHI <vat200@yandex.ru>
2023-12-07 14:19:07 +0300

    change CORS

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM python:3.11-slim
 
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+ENV PYTHONPATH "${PYTHONPATH}:/app"
+
 WORKDIR /app
 
 COPY requirements.txt .
@@ -9,4 +13,5 @@ RUN pip install -r requirements.txt --no-cache-dir
 
 COPY . .
 
-CMD ["uvicorn", "api.main:app", "--proxy-headers", "--host", "0.0.0.0 ", "--port", "8001"]
+CMD ["uvicorn", "api.main:app", "--host", "0.0.0.0", "--port", "8001"]
+
--- api/main.py
+++ api/main.py
@@ -7,18 +7,12 @@ from .model import model_job
 
 
 origins = [
-    "http://localhost",
-    "http://localhost:8080",
-    "http://localhost:8001",
-    "http://localhost:5173",
-    "http://127.0.0.1:8080",
-    "http://127.0.0.1:8001",
-    "http://127.0.0.1:5173",
-    "http://127.0.0.1:1",
-    "http://localhost:80",
-    "http://127.0.0.1:80",
-    "http://localhost:8000",
-    "http://127.0.0.1:8000"
+    "http://proseptmatching.zapto.org:3000",
+    "http://proseptmatching.zapto.org:8080",
+    "http://proseptmatching.zapto.org:8001",
+    "http://proseptmatching.zapto.org:8000",
+    "http://proseptmatching.zapto.org:5173",
+    "http://proseptmatching.zapto.org"
 ]
 
 app = FastAPI()
